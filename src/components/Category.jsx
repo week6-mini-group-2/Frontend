b767636e7e531a096bc7a97de5b2fc7ca3f9b146
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2022-10-22 01:26:08 +0900

    Category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,43 @@
 import React from "react";
-import styled from "styled-components";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Category = () => {
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../css/category.scss";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+export default function Category() {
   return (
-    <StCategoryContainer>
-      <StCategory>
-        <img src="" />
-      </StCategory>
-    </StCategoryContainer>
+    <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        slidesPerGroup={3}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
   );
-};
-
-Category.propTypes = {};
-
-export default Category;
-
-const StCategoryContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const StCategory = styled.div`
-  border-radius: var(--radius-base);
-`;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import "../App.scss";
+import Category from "../components/Category";
 import Header from "../components/Header";
+
 import "../css/variable.scss";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Category />
     </>
   );
 };
