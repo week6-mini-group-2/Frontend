9adda435f0e7a49a5c1e7ccb9a5404bacae95187
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-10-22 00:04:29 +0900

    backup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,14 @@ import React from "react";
 import styled, { css } from "styled-components";
 import "../App.scss";
 import "../css/variable.scss";
+import MasonryGrid from "../components/MasonryGrid";
 
 const Home = () => {
   return (
     <div>
-      <Ex>home</Ex>
+      <MasonryGrid />
     </div>
   );
 };
 
 export default Home;
-
-const Ex = styled.div`
-  width: 200px;
-  height: 650px;
-  background-color: transparent;
-  border-radius: 12px;
-  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.61);
-`;
