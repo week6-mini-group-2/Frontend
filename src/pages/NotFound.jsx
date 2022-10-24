212bd1eb74e3cda8e9b024e43bf700e7bbca24d3
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-10-24 13:12:23 +0900

    404page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const NotFound = () => {
+  return (
+    <div
+      className="notfound"
+      style={{ fontSize: "20rem", textAlign: "center" }}>
+      404 error!
+    </div>
+  );
+};
+
+export default NotFound;
