22399cdc6078fc520d33effca99bc47e91b27835
buddle6091 <83018778+buddle6091@users.noreply.github.com>
2022-10-26 02:20:41 +0900

    grid 선택자 수정

--- src/components/MasonryGrid.jsx
+++ src/components/MasonryGrid.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "../css/variable.scss";
+import styled from "styled-components";
 import Box from "@mui/material/Box";
 import Masonry from "@mui/lab/Masonry";
 import { useSelector, useDispatch } from "react-redux";
@@ -27,19 +28,19 @@ const MasonryGrid = () => {
       <Box style={{ width: "90rem", minHeight: "60rem" }}>
         <Masonry columns={6} spacing={3} id="hoverAni">
           {posts?.map((item) => (
-            <div key={item.postId}>
-              <div className="masonryContents">
-                <img
-                  src={`${item.imageUrl}?w=162&auto=format`}
-                  srcSet={`${item.imageUrl}?w=162&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                  loading="lazy"
-                  style={{
-                    borderRadius: 7,
-                    width: "100%",
-                    boxShadow: "var(--grid-shadow)",
-                  }}
-                />
+            <div className="masonryContents">
+              <img
+                src={`${item.imageUrl}?w=162&auto=format`}
+                srcSet={`${item.imageUrl}?w=162&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+                style={{
+                  borderRadius: 7,
+                  width: "100%",
+                  boxShadow: "var(--grid-shadow)",
+                }}
+              />
+              <div className="masonryBehind">
                 <div className="masonryLabel">{item.title}</div>
               </div>
             </div>
@@ -51,3 +52,24 @@ const MasonryGrid = () => {
 };
 
 export default MasonryGrid;
+
+/* const Temp = styled.div`
+  width: 200px;
+  height: 300px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url(${(props) => props.imageUrl});
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  overflow: hidden;
+  text-align: center;
+  color: whitesmoke;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.911);
+  box-shadow: var(--grid-shadow);
+  border-radius: 7px;
+`; */
+
+/*   <div key={item.postId}>
+              <Temp imageUrl={item.imageUrl}></Temp> */
